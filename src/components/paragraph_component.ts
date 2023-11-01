2fe699a0b7b42ebaac7b639259ceaf5bf55fd9bd
Jay Aljelo Ting <65202977+jayasting98@users.noreply.github.com>
2023-11-01 16:17:31 +0800

    Add a JSDoc description for the paragraph component

--- src/components/paragraph_component.ts
+++ src/components/paragraph_component.ts
@@ -1,5 +1,9 @@
 import {Component} from '../prompts/prompt';
 
+/**
+ * This contains text that formatted to have a trailing newline.
+ * Any leading and trailing white space is removed before adding the trailing newline.
+ */
 export class ParagraphComponent implements Component {
   constructor(protected paragraph: string) {}
 
